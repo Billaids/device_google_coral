6fdab1c7559bbb32fbc00a6faa9ba1f0ccac1afc
Billaids <jimmy.nelle@hsw-stud.de>
2020-03-25 12:31:37 +0100

    coral: use artemis kernel

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -54,11 +54,10 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_SOURCE := kernel/google/floral
-TARGET_KERNEL_CONFIG := floral_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/coral
+TARGET_KERNEL_CONFIG := artemis_coral_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
 TARGET_COMPILE_WITH_MSM_KERNEL := true
- TARGET_KERNEL_CLANG_VERSION := $(shell sh -c "find prebuilts/clang/host/linux-x86/ -type f -name clang -printf \"%T@ %Tc %p\n\" | sort -n | tail -n1 | sed 's/.*clang-\(.*\)\/bin.*/\1/'")
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
